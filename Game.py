96d9d8dd35cffb06516ad237c5c5a36d8c876f87
KingCK18 <christian.kirby2023@gmail.com>
2025-08-08 02:05:18 -0500

    Update README

---
+++ Game.py
@@ -0,0 +1,129 @@
+import os
+import random
+import math
+import pygame
+from os import listdir
+from os.path import isfile, join
+
+pygame.init()
+
+pygame.display.set_caption("The Wild Adventures of the Pink Man")
+
+WIDTH, HEIGHT = 1280, 800
+FPS = 60
+PLAYER_VEL = 5
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Handles the flipping of sprites
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+class Player(pygame.sprite.Sprite):
+    COLOR = (255, 0, 0)
+    GRAVITY = 1
+
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+        self.fall_time = 0
+    
+    # MOVEMENT FUNCTIONS
+        
+    
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+    
+    
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
+        
+
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+    
+
+    def loop(self, fps):
+        self.y_vel += min(1, (self.fall_time / fps) * self.GRAVITY)
+        self.move(self.x_vel, self.y_vel)
+
+        self.fall_time += 1
+    
+    # Draws Player to the screen
+    def draw(self, window):
+        pygame.draw.rect(window, self.COLOR, self.rect )
+
+
+def handle_move(player):
+    keys = pygame.key.get_pressed()
+
+    player.x_vel = 0
+    if keys[pygame.K_LEFT]:
+        player.move_left(PLAYER_VEL)
+    if keys[pygame.K_RIGHT]:
+        player.move_right(PLAYER_VEL)
+
+# Grabs the desired background image tile, 
+# size adjust to fit the window, 
+# and loads the positions with proper spacing into an array 
+def get_background(name):
+    image = pygame.image.load(join("game assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    bg_tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            bg_tiles.append(pos)
+    return bg_tiles, image
+
+
+# Draws images to the screen
+def draw(window, background, bg_image, player):
+    for tile in background:
+        window.blit(bg_image, tile)
+    
+    player.draw(window)
+    
+    pygame.display.update()
+
+
+# Event Loop
+def main(window):
+    clock = pygame.time.Clock()
+    background, bg_image =  get_background("Purple.png")
+
+    player = Player(100, 100, 50, 50)
+
+    run = True
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+        
+        player.loop(FPS)
+        handle_move(player)
+        draw(window, background, bg_image, player)
+
+
+    pygame.quit()
+    quit()
+
+
+
+if __name__ == "__main__":
+    main(window)
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
- # The-Wild-Adventures-of-the-Pink-Man# The-Wild-Adventures-of-the-Pink-Man
+ # The-Wild-Adventures-of-the-Pink-Man
